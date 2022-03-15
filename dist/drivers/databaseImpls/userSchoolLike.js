63fdc1eb55195491351f1607b63b3d23824d99cf
DrPoppyseed <peaske16180@gmail.com>
2022-03-15 13:29:33 +0900

    adjust config

--- dist/drivers/databaseImpls/userSchoolLike.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var UserSchoolLikeSchema = new mongoose_1.Schema({
-    userId: String,
-    ipeds_unitid: String,
-}, { timestamps: true });
-exports.default = (0, mongoose_1.model)('UserSchoolLike', UserSchoolLikeSchema);
